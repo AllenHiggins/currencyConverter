ef3bf9ad11c24996a7fa71b97f3ef2f8dea93f74
Allen Higgins <32021033+AllenHiggins@users.noreply.github.com>
2018-05-12 14:28:51 +0100

    Add files via upload

---
+++ Practical1_2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Practical1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "",
+            "Dollar",
+            "Euro",
+            "Pound"});
+            this.comboBox1.Location = new System.Drawing.Point(73, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(81, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "",
+            "Dollar",
+            "Euro",
+            "Pound"});
+            this.comboBox2.Location = new System.Drawing.Point(188, 83);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(84, 21);
+            this.comboBox2.Sorted = true;
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(81, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "To";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(40, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(268, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(29, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "C";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 171);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Currency Conversion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Practical1_2/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//Allen Higgins C00197373
+
+namespace Practical1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //define a global var
+        private static double amount = 0.0;
+        
+        private void ErrorMsg()
+        {
+            //method for any errors 
+            MessageBox.Show("Please enter a value for all fields!", "Invalied Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if the same value for comboBox2 == comboBox1
+            if (comboBox2.SelectedIndex == comboBox1.SelectedIndex)
+            {
+                //set comboBox2 to index zero - empty string
+                comboBox2.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if comboBox1's value is the same as comboBox2
+            if (comboBox1.SelectedIndex == comboBox2.SelectedIndex)
+            {
+                //then set comboBox1 index to zero - empty string
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if textbox value is null or any comboBox values are at index zero - empty string
+            if (textBox1.Text == "" || comboBox2.SelectedIndex == 0 || comboBox1.SelectedIndex == 0)
+            {
+                //call the error message method
+                ErrorMsg();
+            }
+            else 
+            {
+                //otherwise try store the value as a duoble
+                try
+                {
+                    amount = double.Parse(textBox1.Text);
+                }
+                catch(Exception)
+                {
+                    //else the value was not a number, reset the textBox and call the error msg method
+                    textBox1.Text = "";
+                    ErrorMsg();
+                }
+
+                //store the string value of each selection into one string
+                string convert = comboBox1.Text + " " + comboBox2.Text;
+                //use this string to decide which case is to be carried out
+                switch (convert)
+                {
+                    case "Euro Dollar":
+                        amount = amount * 1.11624;
+                    break;
+                    case "Euro Pound":
+                        amount = amount / 0.85355;
+                    break;
+                    case "Dollar Pound":
+                        amount = amount / 0.76439;
+                    break;
+                    case "Dollar Euro":
+                        amount = amount / 0.89589;
+                    break;
+                    case "Pound Dollar":
+                    amount = amount * 1.30775;
+                    break;
+                    case "Pound Euro":
+                        amount = amount * 1.17157;
+                    break;
+                }
+                //set the textBox to the result of the choosen case rounded to 2 deciemal places
+                textBox1.Text = Math.Round(amount,2).ToString();
+                //change the value of the label to "returns"
+                label1.Text = "returns";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //reset all fields
+            textBox1.Text = "";
+            label1.Text = "Amount";
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            amount = 0.0;
+        }    
+    }
+}
